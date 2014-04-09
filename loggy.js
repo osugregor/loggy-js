e056bf3bfc054aad9c63b8544aeb83219f097763
Gregory Shayko <greg@shaykos.com>
2014-04-09 14:06:04 -0400

    Update loggy.js
    Moved the class into its own object.

--- loggy.js
+++ loggy.js
@@ -1,29 +1,38 @@
-var Loggy = function(){
-	this._DEBUG = 0; this._INFO = 1; this._ERROR = 2;
-	this.level = this._DEBUG;
-	this.levels = ["DEBUG","INFO","ERROR"];
-	this.colors = ['#C74800', 'blue', 'red'];
+var Loggy = {
+	DEBUG: 0,
+	INFO: 1,
+	ERROR: 2,
+	levels: ["DEBUG",	"INFO",	"ERROR"],
+	colors: ['#C74800', 'blue', 'red'],
+	log: function(level){
+		this.level = level || Loggy.DEBUG
+	}
 }
-Loggy.prototype.log = function(messages, level){
+
+Loggy.log.prototype.log = function(messages, level){
 	arguments = Array.prototype.slice.call(arguments, 0);
 	var level = arguments.pop();
-	if(typeof level == 'string')level = this["_" + level.toUpperCase()] || console.error("INVALID LOG LEVEL.");
+	if(typeof level == 'string')level = Loggy[level.toUpperCase()] || console.error("INVALID LOG LEVEL.");
 	if(level < this.level)return;
 	
 	var timestamp = new Date();	
 	var timestampText = ('0' + timestamp.getHours()).slice(-2) + ':' + ('0' + timestamp.getMinutes()).slice(-2) + ':' + ('0' + timestamp.getSeconds()).slice(-2);	
-	var args = ["%c" + timestampText + " | %c" + this.levels[level] + "%c |", "color: black;", "color: "+this.colors[level]+";", "color: black;"];
-	console.log.apply(console, args.concat(arguments));
+	var args = ["%c" + timestampText + " | %c" + Loggy.levels[level] + "%c |", "color: black;", "color: "+Loggy.colors[level]+";", "color: black;"];
+	args = args.concat(arguments);
+	console.log.apply(console, args);
 }
-Loggy.prototype.debug = function(message){
-	var args = Array.prototype.slice.call(arguments);args.push(this._DEBUG);
+
+Loggy.log.prototype.debug = function(message){
+	var args = Array.prototype.slice.call(arguments);args.push(Loggy.DEBUG);
 	this.log.apply(this, args);
 }
-Loggy.prototype.info = function(message){
-	var args = Array.prototype.slice.call(arguments);args.push(this._INFO);
+
+Loggy.log.prototype.info = function(message){
+	var args = Array.prototype.slice.call(arguments);args.push(Loggy.INFO);
 	this.log.apply(this, args);
 }
-Loggy.prototype.error = function(message){
-	var args = Array.prototype.slice.call(arguments);args.push(this._ERROR);
+
+Loggy.log.prototype.error = function(message){
+	var args = Array.prototype.slice.call(arguments);args.push(Loggy.ERROR);
 	this.log.apply(this, args);
 }
