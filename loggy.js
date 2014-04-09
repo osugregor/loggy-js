6bc83a302dfbfb8a576965d43bcf8827880c1a7e
Gregory Shayko <greg@shaykos.com>
2014-04-09 13:39:31 -0400

    Update loggy.js
    Removed the loop, colored just the debug level on console output.

--- loggy.js
+++ loggy.js
@@ -1,33 +1,29 @@
 var Loggy = function(){
-	this._ALL = 0; this._DEBUG = 1; this._INFO = 2; this._ERROR = 3;
+	this._DEBUG = 0; this._INFO = 1; this._ERROR = 2;
 	this.level = this._DEBUG;
-	this.levels = ["ALL","DEBUG","INFO","ERROR"];
-	this.log = function(messages, level){
-		if(arguments.length <= 1)return console.error("Invalid parameters, expecting: log(message, LEVEL)");
-		arguments = Array.prototype.slice.call(arguments);
-		var level = arguments.pop();
-		if(typeof level == 'string')level = this["_" + level.toUpperCase()] || console.error("INVALID LOG LEVEL.");
-		if(level < this.level)return;
-		var timestamp = new Date();
-		for (var i = 0; i < arguments.length; i++) {
-			var timestampText = i == 0 ? timestamp.format("HH:MM:ss") : "        ";
-			console[level == this._ERROR ? 'error' : 'log'](timestampText + " | " + this.levels[level] + " |", arguments[i]);
-		}
-	}
-	this.all = function(message){
-		var args = Array.prototype.slice.call(arguments);args.push(this._ALL);
-		this.log.apply(this, args);
-	}
-	this.debug = function(message){
-		var args = Array.prototype.slice.call(arguments);args.push(this._DEBUG);
-		this.log.apply(this, args);
-	}
-	this.info = function(message){
-		var args = Array.prototype.slice.call(arguments);args.push(this._INFO);
-		this.log.apply(this, args);
-	}
-	this.error = function(message){
-		var args = Array.prototype.slice.call(arguments);args.push(this._ERROR);
-		this.log.apply(this, args);
-	}
+	this.levels = ["DEBUG","INFO","ERROR"];
+	this.colors = ['#C74800', 'blue', 'red'];
+}
+Loggy.prototype.log = function(messages, level){
+	arguments = Array.prototype.slice.call(arguments, 0);
+	var level = arguments.pop();
+	if(typeof level == 'string')level = this["_" + level.toUpperCase()] || console.error("INVALID LOG LEVEL.");
+	if(level < this.level)return;
+	
+	var timestamp = new Date();	
+	var timestampText = ('0' + timestamp.getHours()).slice(-2) + ':' + ('0' + timestamp.getMinutes()).slice(-2) + ':' + ('0' + timestamp.getSeconds()).slice(-2);	
+	var args = ["%c" + timestampText + " | %c" + this.levels[level] + "%c |", "color: black;", "color: "+this.colors[level]+";", "color: black;"];
+	console.log.apply(console, args.concat(arguments));
+}
+Loggy.prototype.debug = function(message){
+	var args = Array.prototype.slice.call(arguments);args.push(this._DEBUG);
+	this.log.apply(this, args);
+}
+Loggy.prototype.info = function(message){
+	var args = Array.prototype.slice.call(arguments);args.push(this._INFO);
+	this.log.apply(this, args);
+}
+Loggy.prototype.error = function(message){
+	var args = Array.prototype.slice.call(arguments);args.push(this._ERROR);
+	this.log.apply(this, args);
 }
